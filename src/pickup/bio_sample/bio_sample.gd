ede9ca82b69aa58b294bf78a6c81fa95398efb8f
streq <strequena@gmail.com>
2023-02-04 20:33:22 -0300

    intento de fix de conteo de biosample

--- src/pickup/bio_sample/bio_sample.gd
+++ src/pickup/bio_sample/bio_sample.gd
@@ -4,8 +4,11 @@ extends Node2D
 signal collected()
 
 onready var panel: Sprite = $panel
-
+var collected = false
 func collect():
+	if collected:
+		return
+	collected = true
 	emit_signal("collected")
 	SFX.play("bio_sample",global_position)
 	queue_free()
